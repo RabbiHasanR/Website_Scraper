59ca6e12417f68ed7aa69c7bbe6bd8d35d7e1b60
Rabbi50 <jasrabbi50@gmail.com>
2020-09-20 00:37:31 +0600

    successfully scrap all info from site and save it file

--- scraper.py
+++ scraper.py
@@ -7,7 +7,6 @@ class Scraper():
     def __init__(self,url):
         self.url=url
 
-
     def get_all_course_link(self,response):
         soup = BeautifulSoup(response.content,features="html.parser")
         atag = soup.find("li", {"class": "menu-item menu-item-type-custom menu-item-object-custom menu-item-723369"}).find('a')
@@ -33,25 +32,15 @@ class Scraper():
         referer_agent_response=request.Request(self.url).get_response_using_referer()
         if referer_agent_response['referer'] is not None:
             link=self.get_all_course_link(referer_agent_response['referer'])
-            #print(link)
             course_categorys=self.get_course_category(link)
-            print(course_categorys)
-            # if course_categorys:
-            #     #print(course_categorys)
-            #     root_path=self.create_directory()
-            #     for category in course_categorys:
-            #         if category:
-            #             category_name=category['category_name'].strip()
-            #             split_category=category_name.split('/')
-            #             #print(split_category[0])
-            #             category_path=self.create_category_directory(root_path,split_category[0])
-            #             #print(category_path)
-            #             all_courses_by_category=self.get_all_course_by_category(category['href'],split_category[0])
-            #             #print('category:',split_category[0],all_courses_by_category)
-            #             for course in all_courses_by_category:
-            #                 self.create_file(category_path,course['course_header'])
+            if course_categorys:
+                root_path=self.create_directory()
+                for category in course_categorys:
+                    if category:
+                        all_courses_by_category,category_path=self.get_all_course_by_cateory(category['href'],category['category_name'],root_path)
+                        self.save_course_details_file(category_path,all_courses_by_category)
 
-    def get_course_list_div(self):
+    def get_course_list_div(self,link):
         referer_agent_response=request.Request(link).get_response_using_referer()
         if referer_agent_response['referer'] is not None:
             soup=BeautifulSoup(referer_agent_response['referer'].content,features="html.parser")
@@ -72,8 +61,13 @@ class Scraper():
 
 
     
-    def get_course_header_and_details_page_link(self,link,category_name):
-        pass
+    def get_all_course_by_cateory(self,link,category_name,root_path):
+        course_list_div=self.get_course_list_div(link)
+        if course_list_div:
+            category_path=self.create_category_directory(root_path,category_name)
+            all_course_by_category=self.get_course_title_and_details_page_link(course_list_div)
+            return all_course_by_category,category_path
+
         
     
     def get_course_and_furture_details(self,course_content_div):
@@ -88,19 +82,33 @@ class Scraper():
         return details_info
 
     def get_course_description(self,course_content_div):
-        course_desc=course_content_div.find('div',{'class':'wywl_benefits'}).find_all('p')
-        # course_desc=course_benifits.find_all('p')
-        description=''
-        for de in course_desc:
-            description+=de.string
-        return description
+        course_benefits_div=course_content_div.find('div',{'class':'wywl_benefits'})
+        if course_benefits_div:
+            course_desc=course_benefits_div.find_all('p')
+            # course_desc=course_benifits.find_all('p')
+            description=''
+            for de in course_desc:
+                if de is not None and de.string is not None:
+                    description+=de.string
+            return description
+        else:
+            return 'Not found any description.'
         
     def get_learn_info(self,course_content_div):
-        return course_content_div.find('ul',{'class':'learn-mod'}).text
+        learn_info_ul=course_content_div.find('ul',{'class':'learn-mod'})
+        if learn_info_ul:
+            return learn_info_ul.text
+        else:
+            return 'Nothing found for learn info.'
 
     
     def get_benifits_of_course(self,course_content_div):
-        return course_content_div.find('ul',{'class':'learn-benefit'}).text
+        benifits_ul=course_content_div.find('ul',{'class':'learn-benefit'})
+        if benifits_ul:
+            return benifits_ul.text
+        else:
+            return 'Nothing found for course benifits info.'
+
     
     def get_course_lessons(self,course_content_div):
         course_lessons=[]
@@ -111,6 +119,10 @@ class Scraper():
         return course_lessons
 
 
+    def save_course_details_file(self,category_path,all_courses):
+        for course in all_courses:
+            course_info=self.get_course_details(course['href'])
+            self.create_file(category_path,course['course_header'],course_info)
 
 
     def get_course_details(self,link):
@@ -118,18 +130,13 @@ class Scraper():
         if referer_agent_response['referer'] is not None:
             soup=BeautifulSoup(referer_agent_response['referer'].content,features="html.parser")
             course_content_div=soup.find('div',{'class':'course-content clearfix popup-container new-c-content'})
-            #box_single_div=find_div.find_all('div',{'class':'box-single'})
-            # print('details_info:',details_info)
-            # print('course description:',desc)
-            # print('what_you_learn:',what_you_learn)
-            # print('benifits_of_taking_course:',benifits_of_taking_course)
-            # print('course modules:',course_lessons)
-
-
-
-
-
-
+            course_details=self.get_course_and_furture_details(course_content_div)
+            course_description=self.get_course_description(course_content_div)
+            course_learn_info=self.get_learn_info(course_content_div)
+            course_benifits=self.get_benifits_of_course(course_content_div)
+            course_lessons=self.get_course_lessons(course_content_div)
+            return {'course_details':course_details,'course_description':course_description,
+            'course_learn_info':course_learn_info,'course_benifits':course_benifits,'course_lessons':course_lessons}
 
     def create_directory(self):
         '''
@@ -142,29 +149,46 @@ class Scraper():
             os.mkdir(path)
         return path
     
-    def create_category_directory(self,parent_path,directory_name):
+    def create_category_directory(self,parent_path,category_name):
         '''
         create file based on category name
         '''
-        path = os.path.join(parent_path, directory_name)
+        category_name=self.remove_special_character_and_spaces_from_string(category_name)
+        path = os.path.join(parent_path, category_name)
         if not os.path.exists(path):
             os.mkdir(path)
         return path
 
-    def create_file(self,path,file_name):
+    def remove_special_character_and_spaces_from_string(self,giveString):
+        modifyString = giveString.translate ({ord(c): " " for c in "!@#$%^&*()[]{};:,./<>?\|`~-=_+"}) #replace all special charecter from category name
+        modifyString=modifyString.replace(' ','') # replace all whitespace from category name
+        return modifyString
+
+
+    def create_file(self,path,file_name,course_info):
         '''
         create file based on course name
         '''
-        file_name = file_name.translate ({ord(c): " " for c in "!@#$%^&*()[]{};:,./<>?\|`~-=_+"}) #replace all special charecter from category name
-        file_name=file_name.replace(' ','') # replace all whitespace from category name
+        file_name=self.remove_special_character_and_spaces_from_string(file_name)
         if not os.path.exists(path+'\\'+file_name):
-            with open(path.strip()+'\\'+file_name+'.txt', 'w') as f:
-                f.write(file_name)
+            with open(path.strip()+'\\'+file_name+'.txt', 'w',encoding="utf-8") as f:
+                f.write('Course details \n')
+                if course_info['course_details']:
+                    for info in course_info['course_details']:
+                        f.write(info+"\n")
+                f.write('\nCourse Description\n')
+                f.write(course_info['course_description']+'\n')
+                f.write('\n'+course_info['course_learn_info']+'\n')
+                f.write('\n'+course_info['course_benifits']+'\n')
+                f.write('\nCourse Lessons\n')
+                if course_info['course_lessons']:
+                    for lesson in course_info['course_lessons']:
+                        f.write(lesson+'\n')
+
                 f.close()
+
 
 if __name__ == "__main__":
     url=input('Enter Url:')
     scraper=Scraper(url)
     scraper.get_courses()
-    # scraper.get_course_details(url)
-    #scraper.create_directory()
